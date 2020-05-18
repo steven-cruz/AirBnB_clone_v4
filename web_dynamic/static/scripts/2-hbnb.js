63ed7be27cf5c8ffe083558ebdff99a10b7b9e15
steven-cruz <stevencruz.300008@gmail.com>
2020-05-18 23:48:09 +0000

    tasks 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,4 +13,12 @@ $(document).ready(function () {
       $('div.amenities > h4').html('&nbsp;');
     }
   });
+
+  $.get('Request http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+   if (textStatus === 'success') {
+     $('div#api_status').addClass('available');
+   } else {
+     $('div#api_status').removeClass('available');
+   }
+  });
 });
