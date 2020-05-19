60ca9d8b3bc3401a6533596be921575ce191e7d9
JuanDavidDava2 <juandm417@gmail.com>
2020-05-19 03:04:02 +0000

    fix id

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid4
+import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
